1d7c06f086e197b7cb4c55a3d5f954ec7e76a698
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2025-09-16 23:13:53 -0400

    Upgraded Framework.  Refactored the Tests directory.

--- Source/Main.js
+++ Source/Main.js
@@ -1,6 +1,9 @@
 "use strict";
 class Program {
     start() {
+        var sampleTests = new SampleTests();
+        var testSuite = TestSuite.fromTestFixtures([sampleTests]);
+        testSuite.run();
         var name = "GameStub";
         var contentDirectoryPath = Configuration.Instance().contentDirectoryPath;
         new Game(name, contentDirectoryPath).start();
